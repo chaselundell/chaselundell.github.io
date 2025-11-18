ce0c014be9abe6b1ac2f9b511b25ec4a925ecff5
Chase Lundell <chaselundell@gmail.com>
2025-11-18 12:42:21 -0700

    fixing

--- wdd131/scripts/temples.js
+++ wdd131/scripts/temples.js
@@ -1,9 +1,9 @@
-const nav = document.querySelector('nav');
+const nav = document.querySelector('.navigation');
 const menuButton = document.querySelector('#menu-button');
 const header = document.querySelector('header');
 
 menuButton.addEventListener('click', () => {
-    header.classList.toggle('open');
+    nav.classList.toggle('open');
     
     const isExpanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
     menuButton.setAttribute('aria-expanded', !isExpanded);
